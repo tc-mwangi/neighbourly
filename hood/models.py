b41e223fb8169c5fe488bbade0a2cc459b7e656e
tc-mwangi <wanjirumwangi.learn@gmail.com>
2019-03-21 16:06:20 +0300

    add class methods

--- hood/models.py
+++ hood/models.py
@@ -49,7 +49,7 @@ class Profile(models.Model):
         [type] -- [description]
     '''
 
-    username = models.OneToOneField(User,on_delete=models.CASCADE,null=True)
+    occupant = models.OneToOneField(User,on_delete=models.CASCADE,null=True)
     avatar = models.ImageField(upload_to='avatar/', null=True)
     bio = models.TextField(max_length=255)
     hood = models.ForeignKey(Hood, on_delete=models.CASCADE, null=True)
@@ -82,7 +82,7 @@ class Hood(models.Model):
     '''
     name = models.CharField(max_length=50, null=True)
     location = models.OnetoManyField(locations, null=True)
-    occupants = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    occupant = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     police_details = models.ForeignKey(Police)
     health_details = models.ForeignKey(Health)
     admin = models.ForeignKey(Admin, on_delete=models.CASCADE, null=True)
@@ -91,16 +91,61 @@ class Hood(models.Model):
     def __str__(self):
         return self.name
 
-    def create_neighbourhood
+    def create_hood(self):
+        self.save()
 
+    def delete_hood(self):
+        self.delete()
 
+    def update_hood(self):
+        self.update()
 
+    
+    @classmethod
+    def find_hood(hood_id):
+        hood = Hood.objects.filter(id=hood_id)
+        return hood
 
+    @classmethod
+    def update_occupants(cls):
+        occupants= cls.objects.get_all()
+        return occupants
 
-
-class Profile(models.Model):
 
 
 
 
 class Business(models.Model):
+    '''creates instances of businesses listed in a neighbourhood
+    
+    Arguments:
+        models {[type]} -- [description]
+    '''
+    name = models.CharField(max_length=50, null=True)
+    hood = models.OnetoMany(Hood, null=True)
+    occupant = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    
+   
+
+
+    def __str__(self):
+        return self.name
+
+    def create_business(self):
+        self.save()
+
+    def delete_business(self):
+        self.delete()
+
+    
+    @classmethod
+    def search_business(cls,business_id):
+        business= cls.objects.filter(id=business_id)
+        return business
+        
+
+
+    @classmethod
+    def update_business(cls):
+        occupants= cls.objects.get_all()
+        return occupants
